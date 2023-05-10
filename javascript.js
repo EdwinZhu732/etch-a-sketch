84e725c9d722f685a3d4fd39b264dea5601dfc4d
EdwinZhu732 <edwinzhu595@gmail.com>
2023-05-10 14:40:30 -0400

    Added working hover effect

--- javascript.js
+++ javascript.js
@@ -3,10 +3,14 @@ const divArray = [];
 const grid = document.querySelector('#grid');
 
 for (let i = 0; i < size * size; i++){
-    var square = document.createElement('div');
+    let square = document.createElement('div');
     square.classList.add('square');
     square.style.width = `${512/size - 2}px`;
     square.style.height = `${512/size - 2}px`;
+    square.style.border = '1px solid gray';
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    })
     grid.appendChild(square);
     divArray.push(square);
 }
